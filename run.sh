5e1ccadf95ad6f14b18a94197d0fe59114a75d6c
Meer Sawood <msawood@redhat.com>
2021-04-12 00:12:54 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,11 +8,7 @@ export AUTH_TOKEN=${THREE_SCALE_KEY};
 export CRDA_KEY=${CRDA_KEY};
 export HOST=https://f8a-analytics-preview-2445582058137.staging.gw.apicast.io
 
-pushd $HOME/.crda
-
-printf 'auth_token: %s\ncrda_key: %s\nhost: %s' "${AUTH_TOKEN}" "${CRDA_KEY}" "${HOST}" >> config.yaml
-
-popd 
+printf 'auth_token: %s\ncrda_key: %s\nhost: %s' "${AUTH_TOKEN}" "${CRDA_KEY}" "${HOST}" >> $HOME/.crda/config.yaml
 
 cat $HOME/.crda/config.yaml
 
