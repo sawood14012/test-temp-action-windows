f1078b745b9cdacbf71c73dd088ecca36debfaf1
Meer Sawood <msawood@redhat.com>
2021-04-12 00:00:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ export HOST=https://f8a-analytics-preview-2445582058137.staging.gw.apicast.io
 
 pushd $HOME/.crda
 
-printf 'auth_token: %s\ncrda_key: %s\n host: %s' "${AUTH_TOKEN}" "${CRDA_KEY}" "${HOST}" >> config.yaml
+printf 'auth_token: %s\ncrda_key: %s\nhost: %s' "${AUTH_TOKEN}" "${CRDA_KEY}" "${HOST}" >> config.yaml
 
 popd 
 
