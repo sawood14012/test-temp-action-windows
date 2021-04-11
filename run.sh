5482329670fa5bddaa19e2df0e44b8d8fc3c84c7
Meer Sawood <msawood@redhat.com>
2021-04-11 23:38:29 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
-mvn --help
-go help
-python3 -V
-node -v
+mkdir -p $HOME/.crda
+
+auth_token = $1  #3e42fa66f65124e6b1266a23431e3d08
+crda_key= $2  #d931dd95-ab1f-4f74-9a9f-fb50f60e4ea9
+host = https://f8a-analytics-preview-2445582058137.staging.gw.apicast.io
+
+pushd $HOME/.crda
+
+printf "auth_token: $(auth_token)\ncrda_key: $(crda_key)\n host: $(host)" >> config.yaml
+
+popd 
+
+cat $HOME/.crda/config.yaml
